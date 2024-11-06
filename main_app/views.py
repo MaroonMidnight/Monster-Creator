68675560522eae313de8c1aa792f96852f1f942b
christiangit <colvera1218@gmail.com>
2024-11-06 15:21:01 -0600

    Put min and max to stats and the sum

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Monster, Moves
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your views here.
 
@@ -22,6 +23,45 @@ def monster_detail(request, monster_id):
 class MonsterCreate(CreateView):
     model = Monster
     fields = ['name', 'species', 'color', 'passive', 'attack', 'speed', 'defense']
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        
+        # Access the form field and set min and max attributes
+        form.fields['attack'].widget.attrs.update({
+            'min': 0,  # Set the minimum value
+            'max': 10,  # Set the maximum value
+        })
+        form.fields['speed'].widget.attrs.update({
+            'min': 0,  # Set the minimum value
+            'max': 10,  # Set the maximum value
+        })
+        form.fields['defense'].widget.attrs.update({
+            'min': 0,  # Set the minimum value
+            'max': 10,  # Set the maximum value
+        })
+
+        return form
+
+        
+    def form_valid(self, form):
+        # Access POST data
+        attack_data = form.cleaned_data.get('attack') # x3
+        speed_data = form.cleaned_data.get('speed')
+        defense_data = form.cleaned_data.get('defense')
+        # Perform operations with the data
+        print(f"Received: {attack_data}") 
+        
+        total = attack_data + speed_data + defense_data
+        
+        if total > 10 or total < 10:
+            # Add an error if the value is 10 or below
+            form.add_error('attack', 'the sum of Attack, Speed and Defense must be equal to 10')
+            return self.form_invalid(form)  # Re-render form with error message
+        
+        # Save the form and continue with the normal workflow
+        return super().form_valid(form)
+
     
 class MonsterUpdate(UpdateView):
     model = Monster
